226ba5e1db70f67d359387e9e32e2420b62ce19c
tonsliy <tonsliy@163.com>
2021-07-20 11:13:13 +0800

    chore(release): publish v0.1.5

--- src/packages/hlzn-design-vue.ts
+++ src/packages/hlzn-design-vue.ts
@@ -2,7 +2,7 @@ import type { App } from 'vue'
 import Button from './button/index'
 
 const components = [Button]
-const version = '0.1.4'
+const version = '0.1.5'
 
     const install = (app: App) => {
       components.forEach((component) => {
